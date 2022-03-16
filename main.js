852188dfc9269cdc288d63197347509e2d2911e9
Tom Strong <strng_thms@yahoo.com>
2022-03-16 17:37:04 -0400

    Form layout, media queries mostly finished, added carousel function

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 // alert('Hello World!')
 // Woohoo made my first line of script
+
+let myIndex = 0;
+let x = document.getElementsByClassName("image-loop");
+
+function carousel() {
+
+    for (let i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+      myIndex = 1
+    }
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 3000);
+}
+
+carousel();
+
+function onSubmit() {
+    alert('Oy!  That feature is coming soon.  Stay tuned!')
+}
