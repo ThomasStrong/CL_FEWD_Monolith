efe82c76f400eab2471a96fbfecde1a5364b50eb
Tom Strong <strng_thms@yahoo.com>
2022-03-16 18:05:08 -0400

    Added alt tags to all images again, working on header layout for desktop media query

--- main.js
+++ main.js
@@ -5,20 +5,19 @@ let myIndex = 0;
 let x = document.getElementsByClassName("image-loop");
 
 function carousel() {
-
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) {
-      myIndex = 1
-    }
-    x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 3000);
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 3000);
 }
 
 carousel();
 
 function onSubmit() {
-    alert('Oy!  That feature is coming soon.  Stay tuned!')
+  alert("Oy!  That feature is coming soon.  Stay tuned!");
 }
